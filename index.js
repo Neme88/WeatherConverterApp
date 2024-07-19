075e07bd5d5acbfd917dc6e69e9187167529357d
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-07-19 23:48:18 +0200

    added a logic that do the calculation and conversation of temperature from Fahrenheit to Celsius

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ function convert(){
     if(toFahrenheit.checked){
         temp =  Number(textBox.value);
         temp = temp * 9 / 5 + 32;
-        result.textContent = temp + "°F";
+        result.textContent = temp.toFixed(1)+ "°F";
     }
     else if(toCelsius.checked){
-        
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5 / 9);
+        result.textContent = temp.toFixed(1) + "°C";
     }
     else{
         result.textContent = "Select a unit";
