9c2a1034e663b191aa26ee4b1c5c4b4f976cc435
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-07-20 00:05:52 +0200

    added comments to enhance readability

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-// TEMPRETURE CONVERSION PROGRAM
+// Function to convert temperature from Celsius to Fahrenheit and vice versa
 
+// Initializing my variables for DOM elements
 const textBox = document.getElementById('textBox');
 const toFahrenheit = document.getElementById('toFahrenheit');
 const toCelsius = document.getElementById('toCelsius');
 const result = document.getElementById('result');
 let temp;
+
+/* 
+This function takes user input and convert it to number 
+and apply the conversion Formular from Celsius to Fahrenheit and vice versa
+*/
 function convert(){
 
     if(toFahrenheit.checked){
