8d8310185e140f7d3843b1cd96871d2e7bbb9d6e
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-07-19 22:43:49 +0200

    Initialized all the const variable needed in our index.js file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // TEMPRETURE CONVERSION PROGRAM
 
+const textBox = document.getElementById('textBox');
+const toFahrenheit = document.getElementById('toFahrenheit');
+const toCelsius = document.getElementById('toCelsius');
+const result = document.getElementById('result');
 function convert(){
-    
+
 }
