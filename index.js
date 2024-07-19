d621f6425e5c6124b37044dc6d6ad75a35981586
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2024-07-19 23:27:20 +0200

    added a logic on our convert function to guild the users to pick one option between two units to convert to.

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const textBox = document.getElementById('textBox');
 const toFahrenheit = document.getElementById('toFahrenheit');
 const toCelsius = document.getElementById('toCelsius');
 const result = document.getElementById('result');
+let temp;
 function convert(){
+
+    if(toFahrenheit.checked){
+
+    }
+    else if(toCelsius.checked){
+
+    }
+    else{
+        result.textContent = "Select a unit";
+    }
 
 }
